a0d1ef7f8d96ef4578e53bb52d135ffb119d3a22
yankikucuk <kendinikertenkelebek@me.com>
2022-10-04 15:50:59 +0300

    Init

---
+++ OmniCD.lua
@@ -0,0 +1,74 @@
+OmniCDDB = {
+  ["profileKeys"] = {
+    ["Leithrien - Silvermoon"] = "Default",
+    ["Faithrien - Silvermoon"] = "Default",
+    ["Jergum - Silvermoon"] = "Default",
+  },
+  ["namespaces"] = {
+    ["LibDualSpec-1.0"] = {
+    },
+  },
+  ["cooldowns"] = {
+  },
+  ["version"] = 2.51,
+  ["global"] = {
+    ["disableElvMsg"] = true,
+  },
+  ["profiles"] = {
+    ["Default"] = {
+      ["Party"] = {
+        ["party"] = {
+          ["extraBars"] = {
+            ["interruptBar"] = {
+              ["locked"] = true,
+            },
+          },
+          ["icons"] = {
+            ["showTooltip"] = true,
+            ["desaturateActive"] = true,
+          },
+          ["position"] = {
+            ["attach"] = "TOPLEFT",
+            ["preset"] = "TOPLEFT",
+            ["uf"] = "ElvUI",
+            ["anchor"] = "TOPRIGHT",
+          },
+          ["manualPos"] = {
+            ["interruptBar"] = {
+              ["y"] = 512,
+              ["x"] = 875,
+            },
+          },
+        },
+        ["visibility"] = {
+          ["arena"] = false,
+        },
+      },
+      ["General"] = {
+        ["fonts"] = {
+          ["statusBar"] = {
+            ["font"] = "Poppins-Medium",
+          },
+          ["optionSmall"] = {
+            ["font"] = "Poppins-Medium",
+          },
+          ["anchor"] = {
+            ["font"] = "Poppins-Medium",
+          },
+          ["icon"] = {
+            ["font"] = "Poppins-Medium",
+          },
+          ["option"] = {
+            ["font"] = "Poppins-Medium",
+          },
+        },
+        ["textures"] = {
+          ["statusBar"] = {
+            ["BG"] = "ElvUI Blank",
+            ["bar"] = "ElvUI Blank",
+          },
+        },
+      },
+    },
+  },
+}
